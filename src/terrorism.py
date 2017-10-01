0d21dce3cd480ba6b680900dbd3744bf8864b331
Lodur <fcs4200@gmail.com>
2017-10-01 18:51:38 -0300

    Initial Data Exploration
    Attributes properties and values.
    Util functions.

--- src/terrorism.py
+++ src/terrorism.py
@@ -6,6 +6,24 @@ from IPython.display import HTML
 import warnings
 warnings.filterwarnings('ignore')
 
+def checkColumns(data):
+    print(data.columns.values)
+
+def checkContent(data):
+    print(terror.head())
+    
+def getRowById(data,id):
+    return (data.loc[data['eventid'] == id])
+
+def getFieldEqualsValue(data,field):
+    return ( data.loc[data[field].isnull() == False]['eventid'] )
+
+def getRowLength(data):
+    return len(data.columns)
+
+def getRowCount(data):
+    return len(data.index)
+    
 # Reading Data to Understand Structure
 terror = pd.read_csv('../input/terrorism.csv',encoding='ISO-8859-1')
-print( terror.head() )
+print( getRowCount(terror) )
